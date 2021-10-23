2811def2118687e2fed2fd2ca91124669a808ac6
naotarian <naonao.070294@gmail.com>
2021-10-23 08:48:32 +0000

    APIall_projectでUsermodelリレーション

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -50,9 +50,17 @@ class ApiController extends Controller
     }
     
     public function all_projejct() {
-        $all_project = Project::all();
-        $all_project = json_encode($all_project);
-        
+        $projects = Project::where('status', 1)->get();
+        foreach($projects as $project) {
+            $project->purpose = $this->purposes[$project->purpose];
+            $project->men_and_women = $this->gender[$project->men_and_women];
+            $project->tools = $this->tools[$project->tools];
+            $project->language = $this->languages[$project->language];
+            $project->sub_language = $this->languages[$project->sub_language];
+            $project->year = $project->minimum_years_old . '歳~' . $project->max_years_old . '歳';
+            $project->user = User::where('id', $project->user_id)->first();
+        }
+        $all_project = json_encode($projects);
         return response($all_project);
     }
 }
