4097fc1241c3826b305fb10288731915495b6738
shimai0913 <shimai0913@gmail.com>
2021-10-23 09:42:14 +0000

    カードの表示修正

--- resources/js/components/Atoms/ProjectColumn.js
+++ resources/js/components/Atoms/ProjectColumn.js
@@ -5,17 +5,19 @@ import Typography from '@mui/material/Typography';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import RoomOutlinedIcon from '@mui/icons-material/RoomOutlined';
 import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+import CommentOutlinedIcon from '@mui/icons-material/CommentOutlined';
+
 import { green } from '@mui/material/colors';
 
 const FlexGrid = styled(Grid)`
   display: flex;
   margin: 4px 0;
 `;
+
 const StyledText = styled(Typography)`
   color: gray;
-  margin: 0 4px;
-  font-size: small;
-  margin: 0 4x;
+  margin: 0 8px !important;
+  // font-size: small;
 `;
 
 const ProjectColumn = ({column, text}) => {
@@ -24,6 +26,7 @@ const ProjectColumn = ({column, text}) => {
       { column === "time" && <AccessTimeIcon sx={{ fontSize: 20, color: green[500] }} /> }
       { column === "location" && <RoomOutlinedIcon sx={{ fontSize: 20, color: green[500] }} /> }
       { column === "people" && <PeopleOutlineIcon sx={{ fontSize: 20, color: green[500] }} /> }
+      { column === "purpose" && <CommentOutlinedIcon sx={{ fontSize: 20, color: green[500] }} /> }
       <StyledText>
         {text}
       </StyledText>
--- resources/js/components/Pages/ProjectListPage.js
+++ resources/js/components/Pages/ProjectListPage.js
@@ -25,93 +25,10 @@ const ProjectListPage = () => {
     if (host) {
       let url = `http://${host}/api/all_projejct` // ここのエンドポイントは適当だから検討
       axios.get(url).then(res => {
-        console.log('レスポンス：', res.data)//データ取れてるはず
-        setProjects(res.data) // レスの中に'user'がないから怒られてる)
+        setProjects(res.data)
       });
     }
   }, [host])
-  // サンプルデータ作成
-  // 上のUseEffectでAPIを叩くと、同じ形で全プロジェクトのデータが帰って来ればそのまま使える。
-  let sampleData = [
-    {
-      created_at: "2021-10-13 16:58:25",
-      deleted_at: null,
-      id: 1,
-      language: "Swift",
-      max_years_old: 30,
-      men_and_women: "女性のみ",
-      minimum_experience: 3,
-      minimum_years_old: 18,
-      number_of_application: 2,
-      project_detail: "テスプロ詳細",
-      project_name: "テスプロ",
-      purpose: "学習",
-      remarks: "テスプロ備考",
-      status: 1,
-      sub_language: "JavaScript",
-      tools: "BitBucket",
-      updated_at: "2021-10-13 16:58:25",
-      work_frequency: null,
-      year: "18歳~30歳",
-      work_frequency: "週1~2時間",
-      user_id: 1,
-      user: {
-        age: 20,
-        comment: null,
-        created_at: "2021-10-13 16:35:17",
-        email: "test@test.com",
-        email_verified_at: null,
-        engineer_history: 1,
-        free_url: null,
-        friends_id: null,
-        icon_image: null,
-        id: 1,
-        self_introduction: null,
-        sex: 2,
-        updated_at: "2021-10-13 16:35:17",
-        user_name: "test" ,
-      },
-    },
-    {
-      created_at: "2021-10-16 13:48:17",
-      deleted_at: null,
-      id: 2,
-      language: "GO",
-      max_years_old: 18,
-      men_and_women: "男性のみ",
-      minimum_experience: 1,
-      minimum_years_old: 16,
-      number_of_application: 1,
-      project_detail: "",
-      project_name: "bbb",
-      purpose: "学習",
-      remarks: "",
-      status: 1,
-      sub_language: "Kotlin",
-      tools: "GitLab",
-      updated_at: "2021-10-16 13:48:17",
-      work_frequency: null,
-      year: "16歳~18歳",
-      work_frequency: "週2~3日",
-      user_id: 1,
-      user: {
-        age: 20,
-        comment: null,
-        created_at: "2021-10-13 16:35:17",
-        email: "test@test.com",
-        email_verified_at: null,
-        engineer_history: 1,
-        free_url: null,
-        friends_id: null,
-        icon_image: null,
-        id: 1,
-        self_introduction: null,
-        sex: 2,
-        updated_at: "2021-10-13 16:35:17",
-        user_name: "test",
-      },
-    },
-  ];
 
   return (
     <ContainerGrid>
