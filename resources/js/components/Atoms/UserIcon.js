f999ca3e8aaa289030008fd5b65e5422334b12f3
shimai0913 <shimai0913@gmail.com>
2021-11-09 17:02:42 +0000

    ユーザーアイコン画像表示

--- resources/js/components/Atoms/UserIcon.js
+++ resources/js/components/Atoms/UserIcon.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import Avatar from '@mui/material/Avatar';
 
-const UserIcon = ({ size, img }) => {
+const UserIcon = ({ size, userImg }) => {
   return (
     <Grid>
       {
-        img ? (
-          <Avatar alt = "" src={img} sx={{ width: size, height: size }} />
+        userImg ? (
+          <Avatar alt="" src={userImg} sx={{ width: size, height: size }} />
         ) : (
             <Avatar sx={{ width: size, height: size }}>H</Avatar>
         )
