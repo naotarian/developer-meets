a218ebfcf20de92d931b23f6ef9c864f231a1b72
shimai0913 <shimai0913@gmail.com>
2021-11-10 10:59:57 +0000

    コンソールエラー潰し

--- resources/views/template/base.blade.php
+++ resources/views/template/base.blade.php
@@ -38,18 +38,5 @@
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>
 <script>
-    function toggleNav() {
-  var body = document.body;
-  var hamburger = document.getElementById('js-hamburger');
-  var blackBg = document.getElementById('js-black-bg');
-
-  hamburger.addEventListener('click', function() {
-    body.classList.toggle('nav-open');
-  });
-  blackBg.addEventListener('click', function() {
-    body.classList.remove('nav-open');
-  });
-}
-toggleNav();
 @yield('scripts')
 </script>
