125d00d7aa0275086b7cb604a20a676a5fb2e8d4
naotarian <naotarian19@gmail.com>
2021-11-16 15:52:30 +0900

    レジスト周り修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,14 +22,16 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background: deepskyblue;">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/') }}" style="color: #fff;font-weight: bold;">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+                {{--
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                --}}
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
