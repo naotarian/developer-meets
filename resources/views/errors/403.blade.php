645db21f5daf4e5d6abfb4faa658cde4a1494f0e
naotarian <naotarian19@gmail.com>
2021-11-19 19:33:33 +0900

    404,500のエラー画面作成

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,10 @@
+@extends('template.base')
+@section('individual_stylesheet')
+
+@endsection
+@section('contents')
+
+<div class="contents">
+    403
+</div>
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+@extends('template.base')
+@section('individual_stylesheet')
+
+@endsection
+@section('contents')
+
+<div class="contents">
+    <div class="error_message">
+        <div class="card" style="max-width: 800px;margin: 3rem auto;">
+          <div class="card-body">
+            <p>お探しのページが見つかりませんでした。<br><a href="/">TOPに戻る</a></p>
+          </div>
+        </div>
+    </div>
+</div>
+@endsection
